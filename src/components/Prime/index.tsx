785701111ba0263aea01bf69d1c693ec9aaa2db2
VoXzE <sinncere520@gmail.com>
2021-04-15 11:21:49 -0400

    feat: created Prime component

---
+++ src/components/Prime/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Prime = () => {
+  return (
+    <div className="prime-box">
+      Enter a number to find nearest prime value
+    </div>
+  )
+}
+
+export default Prime;
