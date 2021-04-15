f80e870139e7ce19b0f29ad340f04a253a466c36
VoXzE <sinncere520@gmail.com>
2021-04-15 12:33:02 -0400

    feat: added status message above grid

--- src/components/Prime/index.tsx
+++ src/components/Prime/index.tsx
@@ -4,6 +4,7 @@ const Prime = () => {
   const [inputNumber, setInputNumber] = useState<number>(0)
   const [gridArr, setGridArr] = useState<number[]>([])
   const [primeNumber, setPrimeNumber] = useState<number | null>(null)
+  const [messageStatus, setMessageStatus] = useState<null | string>(null)
 
   // This is a recursive function checking the value starting
   // from the input value and subtracting by one until we find
@@ -40,8 +41,15 @@ const Prime = () => {
         numArr.push(i+1)
       }
       setGridArr(numArr)
+    } else {
+      setGridArr([])
     }
 
+    if (primeNum === null) {
+      return setMessageStatus(`Could not find any prime numbers before your input.`)
+    }
+
+    setMessageStatus(`${primeNum} is the first prime before your input`)
     setPrimeNumber(primeNum)
   }
 
@@ -60,6 +68,7 @@ const Prime = () => {
         value={inputNumber} 
         onChange={(e) => onInputChange(parseInt(e.target.value))} />
       <button onClick={() => checkInput(inputNumber)}>Check</button>
+      {messageStatus && <p className="status">{messageStatus}</p>}
 
       <div className="prime-grid">
         {gridArr.map((num) => (
